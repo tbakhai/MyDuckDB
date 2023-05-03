fc37a0f3957cd72988bcfecfe36fe9f4db00e8da
Ba Khai Tran <bakhaitran@gmail.com>
2023-05-03 17:50:23 +0200

    mistake

--- publish.py
+++ publish.py
@@ -4,6 +4,6 @@ import duckdb
 con = duckdb.connect('mydatabase.db')
 
 # Query the data from the table
-result = con.execute("SELECT * FROM newtable")
-for row in result.fetchall():
-    print(row)
+#result = con.execute("SELECT * FROM newtable")
+#for row in result.fetchall():
+#    print(row)
