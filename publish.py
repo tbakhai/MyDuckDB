d35851f2420dd338cbed95a16be131cc48f7e0ee
Ba Khai Tran <bakhaitran@gmail.com>
2023-05-03 18:00:01 +0200

    again

--- publish.py
+++ publish.py
@@ -3,11 +3,6 @@ import duckdb
 # Connect to the database
 con = duckdb.connect('mydatabase.db')
 
-import duckdb
-
-# Connect to the database
-con = duckdb.connect('mydatabase.db')
-
 # Query the data from the table
 result = con.execute("SELECT * FROM newtable")
 for row in result.fetchall():
