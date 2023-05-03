e3c570deb3b85f31ef958de9a039f68861d1a066
Ba Khai Tran <bakhaitran@gmail.com>
2023-05-03 18:00:49 +0200

    mistake 2

--- publish.py
+++ publish.py
@@ -2,8 +2,3 @@ import duckdb
 
 # Connect to the database
 con = duckdb.connect('mydatabase.db')
-
-# Query the data from the table
-result = con.execute("SELECT * FROM newtable")
-for row in result.fetchall():
-    print(row)
